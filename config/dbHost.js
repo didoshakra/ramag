13753723c2a35ee406a36977a894acaa4d81f4ce
Roman <didoshakr@ukr.net>
2023-03-12 09:56:03 +0200

    20230312-1

--- config/dbHost.js
+++ config/dbHost.js
@@ -1,3 +1,4 @@
 // Де буде запущена прога: yarn dev -p 3000
-export const dbHost = "http://localhost:3000"
+// export const dbHost = "http://localhost:3000"
 // export const dbHost = "127.0.0.1"
+export const dbHost = "https://ramag.vercel.app"
